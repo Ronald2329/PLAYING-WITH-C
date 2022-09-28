5e7096d91e7abe6268f58f910060fe8ad121450b
Ronald2329 <101905617+Ronald2329@users.noreply.github.com>
2022-09-28 19:54:49 -0300

    Update STRUCT DENTRO DE STRUCT.c

--- STRUCT DENTRO DE STRUCT.c
+++ STRUCT DENTRO DE STRUCT.c
@@ -1,21 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+//Criando strct para contato
 struct st_contato{
 	char nome[100];
 	char email[100];
 	char numero[20];
 };
 
+//Importanto uma struct para dentro de outra struct
+//No caso teremos uma agenda e dentro dessa agenda cada contato terá um nome, email e um número.
 struct st_agenda{
 	struct st_contato contatos[100];
 }agenda;
 
+//Subprograma para entrada de dados
 void entrada_de_dados()
 {
+	//Variáveis locais(de não atribuição)
 	int i;
 	int var;
 	
+	//Nota: foi utilizado fgets com exceção na última pois como veremos abaixo a função fgets coleta sempre um caracter a mais no caso o "/0"
 	printf("-------------------------INICIO DO PROGRAMA-------------------------\n");
 	printf(" Apos a digitacao pressione 2x a tecla enter do contrario digitar\nos dois espacos acarretara o preenchimento vazio na agenda\n");
 	for(i = 0 ; i < 3; i++)
@@ -23,18 +29,22 @@ void entrada_de_dados()
 	printf("Informe o nome do contato:\n"); 
 	fgets(agenda.contatos[i].nome, 100, stdin);
 	getchar();
+        //Getchar para caso o usuario digite errado "não pule" quando voltar para a digitação
 	
 	printf("INforme o e_mail do usuario: \n");
 	fgets(agenda.contatos[i].email, 100, stdin);
 	getchar();
 	
 	printf("INforme o numero do contato: \n");
-    scanf("%s",&agenda.contatos[i].numero);
-    getchar();
+        scanf("%s",&agenda.contatos[i].numero);
+	//Nota: a função scanf exclui o "\0" enquanto a fgets conta mais um como por exemplo:
+	//"Ronald" = 6 caracters enquanto com fgets seria: "Ronald\0" totalizando 7 pois o o "/0" eh um unico caracter especial das finais de cada string"
+        getchar();
 	
+    //Faz a coleta da qquantidade de strings digitadas
     var = strlen(agenda.contatos[i].numero);
     
-    
+        //Verifica a quantidade de strings
 	if(var > 20)
 	{
 		while( var >20)
@@ -52,13 +62,16 @@ void entrada_de_dados()
 		}
 	}
 	
+		//Funções para "Pausar e limpar a tela" respectivamente
 	system("pause");
 	system("cls");
 	}
 }
 
+//Função para exibir dados
 void Exibir_dados()
 {
+	//Variável local(de não atribuição)
 	int i;
 	
 	printf("======================================================AGENDA=======================================================\n");
@@ -73,7 +86,7 @@ void Exibir_dados()
 }
 int main()
 {
-	
+	//Chamada de funções 
 	entrada_de_dados();
 	Exibir_dados();
 }
